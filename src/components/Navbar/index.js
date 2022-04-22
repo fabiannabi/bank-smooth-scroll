20f36de1a9e83956db3811c84e470d9453ef5c14
fabiannabi <fabiannabi@yahoo.com.mx>
2022-04-22 13:54:12 -0500

    Added menu items and button for login
    -Refactors into loop for repeated code
    -Adds new refrence to styled components

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,16 +1,46 @@
-import React from 'react'
-import {Nav, NavBarContainer, NavLogo} from './NavBarStyled'
+import React from "react";
+import { FaBars } from "react-icons/fa";
+import {
+  Nav,
+  NavBarContainer,
+  NavLogo,
+  MobileIcons,
+  NavItem,
+  NavMenu,
+  NavLinks,
+  NavBtn,
+  NavBtnLink,
+} from "./NavBarStyled";
 
 const NavBar = () => {
+  const navList = () => {
+    const menu = ["About", "Discover", "Services", "Sign up"];
+    const list = menu.map((menuItem) => {
+      return (
+        <NavItem key={menuItem}>
+          <NavLinks to={menuItem.toLocaleLowerCase()}>{menuItem}</NavLinks>
+        </NavItem>
+      );
+    });
+    return list;
+  };
+
   return (
     <>
       <Nav>
         <NavBarContainer>
-          <NavLogo to='/'>Cashit</NavLogo>
+          <NavLogo to="/">Cashit</NavLogo>
+          <MobileIcons>
+            <FaBars />
+          </MobileIcons>
+          <NavMenu>{navList()}</NavMenu>
+          <NavBtn>
+            <NavBtnLink to="signin">Sign in</NavBtnLink>
+          </NavBtn>
         </NavBarContainer>
       </Nav>
     </>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
